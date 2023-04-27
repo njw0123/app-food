cb133f456d9cbbe4a35711d35d5492880963b2c4
luffy <luffy@abc.com>
2023-04-27 15:21:31 +0900

    feat:시간별 날씨기능추가

---
+++ src/main/java/data/fcst/FcstResponse.java
@@ -0,0 +1,15 @@
+package data.fcst;
+
+public class FcstResponse {
+	FcstHeader header;
+	FcstBody body;
+
+	public FcstHeader getHeader() {
+		return header;
+	}
+
+	public FcstBody getBody() {
+		return body;
+	}
+	
+}
